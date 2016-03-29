247248808e95caddf658396de8ea356580417cea
Jack the Ripper <andreastheripper@aol.de>
2016-03-29 21:54:16 +0200

    basic range visualization added

---
+++ Assets/Scripts/RangeRenderer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class RangeRenderer : MonoBehaviour
+    {
+        public Color Color;
+        public int Points = 50;
+        public float Width = 1f;
+      
+        protected virtual void Start()
+        {
+            var tower = GetComponentInParent<Tower>();
+            var radius = tower.Range;
+
+            var lineRenderer = GetComponent<LineRenderer>();
+            var points = new Vector3[Points + 1];
+            for (var i = 0; i < Points; i++)
+            {
+                var index = i;
+
+                var angle = 2*Mathf.PI/Points*index;
+
+                points[index] = CalculatePoint(radius, angle);
+            }
+            points[Points] = points[0];
+
+            lineRenderer.SetColors(Color, Color);
+            lineRenderer.SetVertexCount(points.Length);
+            lineRenderer.SetPositions(points);
+            lineRenderer.SetWidth(Width, Width);
+        }
+
+        private static Vector3 CalculatePoint(float radius, float angle)
+        {
+            var x = radius * Mathf.Cos(angle);
+            var z = radius * Mathf.Sin(angle);
+            return new Vector3(x, 0.2f, z);
+        }
+    }
+}
